6d86547ec2147ddaa6df43b5407b00c9508a6889
aasawari5 <aasawari.turna05@gmail.com>
2024-07-17 00:12:56 +0530

    data validation files committed

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.logging import logger
 
-STAGE_NAME = "Data Ingestion satge"
+STAGE_NAME = "Data Ingestion stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} stared <<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\n x============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} stared <<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\n x============x")
 except Exception as e:
     logger.exception(e)
